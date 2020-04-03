574ccb8c4307cada98c493633dd5f77838b14105
Rafael Ortiz <rafael.ortiz.barrera@gmail.com>
2020-04-02 23:10:25 -0300

    Se crea menu interactivo

--- leerTokenComprimido.js
+++ leerTokenComprimido.js
@@ -1,19 +1,32 @@
 var zlib = require('zlib');
-console.log("Ingrese access_token");
-var stdin = process.openStdin();
+var inquirer = require('inquirer');
 
-stdin.addListener("data", function(d) {
-	let token = d.toString().trim();
-    let listaToken = token.split('.');
-    if (listaToken.length != 3){
-    	console.log("El access token no esta completo");
-    }
-    let head = listaToken[0];
-    let body = listaToken[1];
-    let buffer = Buffer.from(body,'base64');
-    let json = zlib.inflateSync(buffer).toString();
-    console.log('\n');
-    console.log('JWT BODY: ' + json);
-    console.log('\n');
-    process.exit();
- });
+
+inquirer.registerPrompt('recursive', require('inquirer-recursive'));
+inquirer.prompt([{
+    type: 'recursive',
+    message: 'deseas descomprimir (DEFLATE) JWT ?',
+    name: 'token',
+    prompts: [
+        {
+            type: 'input',
+            name: 'access_token',
+            message: 'Ingresa access_token',
+            validate: function (value) {
+                let token = value.toString().trim();
+                let listaToken = token.split('.');
+                if (listaToken.length === 3) { 
+                    let head = listaToken[0];
+                    let body = listaToken[1];
+                    let buffer = Buffer.from(body,'base64');
+                    let json = zlib.inflateSync(buffer).toString();
+                    console.log('\n');
+                    console.log('JWT BODY: ' + json);
+                    console.log('\n');
+                    return true;
+                }
+                return 'access_token con formato incorrecto';
+            }
+        },
+    ]
+}]);
